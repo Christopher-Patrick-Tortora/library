a97a7ca2d35533f910277f51ebb32dcc868626b9
Christopher Patrick Tortora <ctortora42@gmail.com>
2023-10-05 14:21:40 -0400

    Update demo books

--- Library.mjs
+++ Library.mjs
@@ -3,25 +3,28 @@ import { Book } from "./Book.mjs";
 const Library = () => {
     const book1 = Book("SlaughterHouse-Five",
         "Kurt Vonnegut",
+        'Postmodern',
         190,
         "English",
         "31 March 1969",
         190,
-        "Read")
-    const book2 = Book("SlaughterHouse-Five",
-        "Kurt Vonnegut",
-        190,
+        true)
+    const book2 = Book("The Great Gatsby",
+        "F. Scott Fitzgerald",
+        'Tragedy',
+        208,
         "English",
-        "31 March 1969",
-        190,
-        "Read")
-    const book3 = Book("SlaughterHouse-Five",
-        "Kurt Vonnegut",
-        190,
+        "10 April 1925",
+        0,
+        false)
+    const book3 = Book("The Subtle Art of Not Giving a F*ck: A Counterintuitive Approach to Living a Good Life",
+        "Mark Manson",
+        'Self-help',
+        272,
         "English",
-        "31 March 1969",
-        190,
-        "Read")
+        "13 September 2016",
+        0,
+        false)
     const collection = [book1, book2, book3]
 
     const addBook = (book) => {
