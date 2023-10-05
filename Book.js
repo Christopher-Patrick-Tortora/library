f49d1f9c14111ee2006ea2dee6b592df49b8af3b
Christopher Patrick Tortora <ctortora42@gmail.com>
2023-10-05 12:44:15 -0400

    Create getters and setters

---
+++ Book.js
@@ -0,0 +1,62 @@
+const Book = (title, author, pageNumber, language, published, pagesRead, stat) => {
+    
+    return {
+        set title(newTitle) {
+            title = newTitle;
+        },
+
+        get title() {
+            return title
+        },
+
+        set author(newAuthor) {
+            author = newAuthor
+        },
+
+        get author() {
+            return author
+        },
+
+        set pageNumber(newPageNumber) {
+            pageNumber = newPageNumber
+        },
+
+        get pageNumber() {
+            return pageNumber
+        },
+
+        set language(newLanguage) {
+            language = newLanguage
+        },
+
+        get language() {
+            return language
+        },
+
+        set published(newPublished) {
+            published = newPublished
+        },
+
+        get published() {
+            return published
+        },
+
+        set pagesRead(newPagesRead) {
+            pagesRead = newPagesRead
+        },
+
+        get pagesRead() {
+            return pagesRead
+        },
+
+        set stat(newStat) {
+            stat = newStat
+        },
+
+        get stat() {
+            return stat
+        }
+        
+    }
+}
+
