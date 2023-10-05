1b97831f81cda5891fd80eecaa52405159648cac
Christopher Patrick Tortora <ctortora42@gmail.com>
2023-10-05 14:20:55 -0400

    Update card information

--- UI.mjs
+++ UI.mjs
@@ -12,18 +12,20 @@ const UI = () => {
     const createBookCard = (book) => {
         const display = document.querySelector('main')
         display.innerHTML += `
-        <section>
+        <section class="book">
             <h3>${book.title}</h3>
             <p>By: ${book.author}</p>
+            <p>Genre: ${book.genre}</p>
             <p>Page #: ${book.pageNumber}</p>
             <p>Language: ${book.language}</p>
             <p>Published: ${book.published}</p>
             <p>Pages read: ${book.pagesRead}</p>
-            <label class="switch">
-                Finished: 
-                <input type="checkbox">
-                <span class="slider"></span>
-            </label>
+            <label for="status">Status: </label>
+            <select name="status" class="status">
+              <option value="Not Started">Not Started</option>
+              <option value="Started">Started</option>
+              <option value="Finished">Finished</option>
+            </select> 
         </section>`
     }
 
